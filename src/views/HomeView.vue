499d73399c3a0a730474d763e60d49b7ac50d6f6
Roger Camargo <huogerac@gmail.com>
2022-09-12 23:14:52 -0300

    Versão inicial da página de listagem

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,33 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bem-vindo ao TODO Vuetify
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Gerenciando tarefas aprendendo VueJS + Vuetify + API Mock
+        </p>
+        <p>
+          <v-btn rounded color="primary" dark :to="{ name: 'taskList' }">
+            Iniciar
+          </v-btn>
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
-
-export default {
-  components: {
-    HelloWorld,
-  },
-}
+export default {}
 </script>
---
+++ src/views/TasksListView.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-main class="overflow-hidden">
+    <v-container>
+      <v-row>
+        <v-col class="pa-1" cols="12" v-for="task in tasks" :key="task.id">
+          <v-card>
+            <v-card-text>
+              <div>#{{ task.id }}</div>
+              <p class="ma-0 pa-0 text-h5 text--primary">{{ task.title }}</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-list-item class="grow">
+                <v-row align="center" justify="end">
+                  <v-btn icon color="grey"><v-icon>fas fa-pen</v-icon></v-btn>
+                  <v-btn icon color="grey"
+                    ><v-icon>far fa-trash-alt</v-icon></v-btn
+                  >
+                </v-row>
+              </v-list-item>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Task 1',
+        },
+        {
+          id: 2,
+          title: 'Task Two',
+        },
+      ],
+    }
+  },
+}
+</script>
