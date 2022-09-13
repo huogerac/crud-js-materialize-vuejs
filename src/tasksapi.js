ca994d3391073f203e2aae2a62e544473117aa2b
Roger Camargo <huogerac@gmail.com>
2022-09-12 22:48:21 -0300

    Versão inicial do Vuetify

--- src/tasksapi.js
+++ src/tasksapi.js
@@ -1,15 +1,15 @@
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = 'http://localhost:3000'
 
 // GET lista de tarefas
 function getTasks(cb) {
   axios
-    .get("/tasks")
+    .get('/tasks')
     .then((response) => {
       cb(response.data)
     })
     .catch((error) => {
-      console.log("error:", error);
-    });
+      console.log('error:', error)
+    })
 }
 
 // GET de uma task específica
@@ -17,11 +17,11 @@ function getTask(taskId, cb) {
   axios
     .get(`/tasks/${taskId}`)
     .then((response) => {
-      cb(response.data);
+      cb(response.data)
     })
     .catch((error) => {
-      console.log("error:", error);
-    });
+      console.log('error:', error)
+    })
 }
 
 // POST para criar uma nova tarefa
@@ -31,15 +31,15 @@ function createTask(title, project, date, cb) {
     title: title,
     project: project,
     dueTo: date,
-  };
+  }
   axios
     .post(`/tasks`, taskData)
     .then((response) => {
-      cb(response.data);
+      cb(response.data)
     })
     .catch((error) => {
-      console.log("erro:", error);
-    });
+      console.log('erro:', error)
+    })
 }
 
 // PUT para atutalizar todos dados da tarefa
@@ -48,30 +48,30 @@ function updateTask(taskId, title, project, date, cb) {
     title: title,
     project: project,
     dueTo: date,
-  };
+  }
   axios
     .put(`/tasks/${taskId}`, taskData)
     .then((response) => {
-      cb(response.data);
+      cb(response.data)
     })
     .catch((error) => {
-      console.log("erro:", error);
-    });
+      console.log('erro:', error)
+    })
 }
 
 // PATCH para atutalizar um dado da tarefa
 function updateTaskDate(taskId, date) {
   const taskData = {
     dueTo: date,
-  };
+  }
   axios
     .patch(`/tasks/${taskId}`, taskData)
     .then((response) => {
-      console.log("Tarefa salva", response.data);
+      console.log('Tarefa salva', response.data)
     })
     .catch((error) => {
-      console.log("erro:", error);
-    });
+      console.log('erro:', error)
+    })
 }
 
 // DELETE para apagar uma tarefa
@@ -79,9 +79,9 @@ function deleteTask(taskId, cb) {
   axios
     .delete(`/tasks/${taskId}`)
     .then((response) => {
-      cb(response.data);
+      cb(response.data)
     })
     .catch((error) => {
-      console.log("erro:", error);
-    });
+      console.log('erro:', error)
+    })
 }
